4dc0b7b0c25c4600bc65ac3205aad49abcd08801
miai.vn <thangnch@gmail.com>
2025-01-10 07:17:21 +0700

    Add files via upload

---
+++ install_noai.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "--------- 🟢 Start install docker -----------"
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+echo "--------- 🔴 Finish install docker -----------"
+echo "--------- 🟢 Start creating folder -----------"
+cd ~
+mkdir vol_n8n
+sudo chown -R 1000:1000 vol_n8n
+sudo chmod -R 755 vol_n8n
+echo "--------- 🔴 Finish creating folder -----------"
+echo "--------- 🟢 Start docker compose up  -----------"
+wget https://raw.githubusercontent.com/thangnch/MIAI_n8n_dockercompose/refs/heads/main/compose_noai.yaml -O compose.yaml
+export EXTERNAL_IP=http://"$(hostname -I | cut -f1 -d' ')"
+docker compose up -d
+echo "--------- 🔴 Finish! Wait a few minutes and test in browser at url $EXTERNAL_IP for n8n UI -----------"
