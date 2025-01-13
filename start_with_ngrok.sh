b9c169240f2fdc3f5cd035379658ac0f60b90eab
miai.vn <thangnch@gmail.com>
2025-01-13 23:30:21 +0700

    Update start_with_ngrok.sh

--- start_with_ngrok.sh
+++ start_with_ngrok.sh
@@ -6,10 +6,11 @@ echo "--------- 🟢 Start Ngrok setup -----------"
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 sudo apt install -y jq
-echo "🔴🔴🔴 Please login into ngrok.com and paste your token here:"
+echo "🔴🔴🔴 Please login into ngrok.com and paste your token and static URL here:"
 read -p "Token : " token
+read -p "Domain : " domain
 ngrok config add-authtoken $token
-ngrok http 80 > /dev/null &
+ngrok http --url=$domain 80 > /dev/null &
 echo "🔴🔴🔴 Please wait Ngrok to start...."
 sleep 5
 export EXTERNAL_IP="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
