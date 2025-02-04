7dfb85477441ef9ffb7be06fef5239c1770b1784
miai.vn <thangnch@gmail.com>
2025-02-04 15:32:25 +0700

    Update start_with_ngrok.sh

--- start_with_ngrok.sh
+++ start_with_ngrok.sh
@@ -17,5 +17,5 @@ export EXTERNAL_IP="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].p
 echo Got Ngrok URL = $EXTERNAL_IP
 echo "--------- 🔴 Finish Ngrok setup -----------"
 echo "--------- 🟢 Start Docker compose up  -----------"
-docker compose up -d
+sudo docker compose up -d
 echo "--------- 🔴 Finish! Wait a few minutes and test in browser at url $EXTERNAL_IP for n8n UI -----------"
