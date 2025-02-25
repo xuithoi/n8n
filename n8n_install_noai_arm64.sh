bb6aef391d303fc303f444cba7205e1f777cde13
miai.vn <thangnch@gmail.com>
2025-02-25 21:15:05 +0700

    Update n8n_install_noai_arm64.sh

--- n8n_install_noai_arm64.sh
+++ n8n_install_noai_arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "--------- 🟢 Start install docker -----------"
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -13,7 +13,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 # apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "--------- 🔴 Finish install docker -----------"
 echo "--------- 🟢 Start creating folder -----------"
 cd ~
